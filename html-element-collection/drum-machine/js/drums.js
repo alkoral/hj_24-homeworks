11c2fcf9f2bb4b3c352b74f303c470ef618f4ffe
alkoral <35643632+alkoral@users.noreply.github.com>
2018-09-10 12:50:39 +0300

    Add files via upload

--- html-element-collection/drum-machine/js/drums.js
+++ html-element-collection/drum-machine/js/drums.js
@@ -4,6 +4,7 @@ const drums = document.getElementsByClassName('drum-kit__drum');
 for (let key of drums) {
 	key.onclick = function() {
 		const sound = key.getElementsByTagName('audio')[0];
+		sound.currentTime = 0;
 		sound.play();
 	}
 }
